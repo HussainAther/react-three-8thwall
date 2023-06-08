691a2830e95acdd199d107ef85a244f2b8b84f54
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 16:01:44 -0400

    try this way of incorporating jsx code instead

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App'
+// import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+import React from 'react';
+import DreiRefraction from './examples';
+
+class ARApp extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Other JSX code for your AR app */}
+        <DreiRefraction />
+      </div>
+    );
+  }
+}
+
+export default ARApp;
