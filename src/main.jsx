4c0d365f143641184043195fa55c47a9d4f3fa29
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 17:32:10 -0400

    running with const App version

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import DreiRefraction from './examples';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+//   render() {
+//     return (
+//       <div>
+//         {/* Other components and UI elements */}
+//         <DreiRefraction />
+//         {/* Additional components and UI elements */}
+//       </div>
+//     );
+//   }
+  
+// );
+
+const App = () => {
+  return (
+    <div>
+      {/* Other components and UI elements */}
+      <DreiRefraction />
+      {/* Additional components and UI elements */}
+    </div>
+  );
+}
+
+export default App;
