a1da78165ad4e1ae3ee622a29c04d8d1ae0e4169
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 15:01:47 -0400

    fixed DreiRefraction import location

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import DreiRefraction from './src';
+import DreiRefraction from './DreiRefraction.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
