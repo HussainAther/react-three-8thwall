d545906fdd67bb0f7514f1ad6a0b928b8e46fed0
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 17:44:00 -0400

    try replacing dreirefraction with app

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import DreiRefraction from './examples';
-
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  <DreiRefraction />
-)
+    <DreiRefraction />
+  </React.StrictMode>
+);
