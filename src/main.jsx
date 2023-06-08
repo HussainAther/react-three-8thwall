44aed6ccba02c10c1973424d16c982f61cc22029
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 18:45:45 -0400

    console log the exports and imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DreiRefraction from './examples.jsx';
+// import DreiRefraction from './examples.js';
+import * as constants from './examples.js';
 import './index.css';
+
+console.log(constants)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
