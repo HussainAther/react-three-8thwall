1e5c91d7f9df307017cac4857f9e42e1bbc1b474
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 16:13:37 -0400

    revert to original

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import DreiRefraction from './examples';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <DreiRefraction />
   </React.StrictMode>
 );
