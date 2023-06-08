2c8015c7090efe74a407904e8fb5fc01f8c21497
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 14:54:33 -0400

    moved dreirefraction to tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import DreiRefraction from './src';
-<DreiRefraction />;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <DreiRefraction />
+  </React.StrictMode>
+);
