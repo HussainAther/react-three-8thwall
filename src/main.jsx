13aaedd7ea9ed6539180076bf5692393b41d7837
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 18:18:18 -0400

    dreirefraction within brackets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DreiRefraction from './examples';
+import { DreiRefraction } from './examples';
 import App from './App';
 import './index.css';
 
