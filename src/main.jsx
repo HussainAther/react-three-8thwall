c7f601707c02aaa4433652be8ce0aa81e57277ac
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 18:50:09 -0400

    direrefraction examples

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import DreiRefraction from './examples.js';
+import DreiRefraction from './examples.js';
 import './index.css';
-import './examples.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
