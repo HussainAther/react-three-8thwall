fe984c055d936b64752c06841868fb80c74d4303
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 15:55:19 -0400

    reset to original main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import DreiRefraction from './examples';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <DreiRefraction />
-  </React.StrictMode>
-);
-
+  </React.StrictMode>,
+)
