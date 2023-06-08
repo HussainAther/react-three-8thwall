53a165efc874adfb30e9c3cc173c439c37b4a2ee
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 18:52:27 -0400

    import/export dreirefraction directly from file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DreiRefraction from './examples.js';
+import DreiRefraction from './DreiRefraction.jsx;
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
