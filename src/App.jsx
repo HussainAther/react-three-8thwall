61e59556cb4416f42a1130251016d764388e7f96
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 19:31:15 -0400

    dreirefraction import change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import XR8Scene from './XR8Scene';
 import { useRef, useEffect } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import { DreiRefraction } from './DreiRefraction.jsx';
+import DreiRefraction from './DreiRefraction';
 
 function App() {
   const canvasRef = useRef();
