8b364b8f4bec04e8504c3a4666968ea947691bbf
Syed Hussain Ather <shussainather@gmail.com>
2023-06-08 18:33:27 -0400

    will it work if i try adding the file extension?

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DreiRefraction from './DreiRefraction';
+import DreiRefraction from './examples.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
