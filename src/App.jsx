2d90452f8c7077fb9b77c66411a5f02df82f57e6
Syed Hussain Ather <shussainather@gmail.com>
2023-06-09 23:26:25 -0400

    menu login buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import XR8Scene from './XR8Scene';
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import './App.css';
 import { Canvas, useThree } from '@react-three/fiber';
 import LoadedGltf from './LoadedGltf';
@@ -8,6 +8,9 @@ import { Html } from '@react-three/drei';
 function App() {
   const canvasRef = useRef();
   const R3Scene = useRef();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setLoggedIn] = useState(false);
 
   let { onxrloaded, cubeCamera } = XR8Scene(canvasRef, R3Scene);
 
@@ -21,10 +24,22 @@ function App() {
     console.log('Object clicked!');
   };
 
-  const handleMenuClick = () => {
-    // Handle menu click event
-    // This can be a placeholder for your menu interaction logic
-    console.log('Menu clicked!');
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    // Validate username and password
+    if (username === 'admin' && password === 'password') {
+      setLoggedIn(true);
+      console.log('Login successful!');
+    } else {
+      console.log('Invalid username or password!');
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+    setUsername('');
+    setPassword('');
   };
 
   return (
@@ -38,7 +53,29 @@ function App() {
         </scene>
         {/* Overlay HTML for UI elements */}
         <Html>
-          <Menu onClick={handleMenuClick} />
+          {isLoggedIn ? (
+            <div className="menu">
+              <h2>Welcome, {username}!</h2>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <form className="login-form" onSubmit={handleLogin}>
+              <h2>Login</h2>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Login</button>
+            </form>
+          )}
         </Html>
       </Canvas>
       <canvas
@@ -52,14 +89,4 @@ function App() {
 }
 
 export default App;
-
-function Menu({ onClick }) {
-  return (
-    <div className="menu">
-      <button onClick={onClick}>Menu Item 1</button>
-      <button onClick={onClick}>Menu Item 2</button>
-      <button onClick={onClick}>Menu Item 3</button>
-    </div>
-  );
-}
 
