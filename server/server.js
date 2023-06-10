a9ba23f9dee7f4f7db40baaa376271b9d6f0700f
Syed Hussain Ather <shussainather@gmail.com>
2023-06-10 00:20:50 -0400

    line readded

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Define the API route for fetching model data based on the model ID
+app.get('/api/models/:modelId', async (req, res) => {
+  const { modelId } = req.params;
+
+  try {
+    const response = await axios.get(`https://api.sketchfab.com/v3/models/${modelId}`);
+    const modelData = response.data;
+
+    res.json(modelData);
+  } catch (error) {
+    res.status(404).json({ error: 'Model not found' });
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
