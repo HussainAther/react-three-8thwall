9ac127fcd5b3ff30e6638e6af24f75d13ef59464
Syed Hussain Ather <shussainather@gmail.com>
2023-06-09 22:53:18 -0400

    back to gltf file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import XR8Scene from './XR8Scene';
-import { useRef, useEffect, useState } from 'react'; // Add useState
+import { useRef, useEffect } from 'react';
 import './App.css';
-import { Canvas, useThree } from '@react-three/fiber'; // Add useThree
-import { Html, useDetectGPU } from '@react-three/drei'; // Add Html and useDetectGPU
-
+import { Canvas } from '@react-three/fiber';
+import LoadedGltf from './LoadedGltf';
 
 function App() {
   const canvasRef = useRef();
   const R3Scene = useRef();
-  const [isMenuVisible, setMenuVisible] = useState(false); // State for menu visibility
 
   let { onxrloaded, cubeCamera } = XR8Scene(canvasRef, R3Scene);
 
@@ -16,35 +14,14 @@ function App() {
     XRExtras.Loading.showLoading({ onxrloaded });
   }, []);
 
-  const handleObjectClick = () => {
-    // Handle object click event
-    // This can be a placeholder for your object interaction logic
-    console.log('Object clicked!');
-  };
-
-  const handleMenuButtonClick = () => {
-    // Toggle menu visibility
-    setMenuVisible(!isMenuVisible);
-  };
-
   return (
     <div className="App">
       <Canvas style={{ position: 'absolute' }}>
         <scene ref={R3Scene}>
           <ambientLight />
           {/* <DreiRefraction envMap={cubeCamera.renderTarget.texture} /> */}
-          <LoadedGltf onClick={handleObjectClick} />
+          <LoadedGltf />;
           <pointLight position={[10, 15, 10]} />
-
-          {/* Display menu only if it's visible */}
-          {isMenuVisible && (
-            <Menu handleObjectClick={handleObjectClick} />
-          )}
-
-          {/* Overlay HTML for UI elements */}
-          <Html>
-            <button onClick={handleMenuButtonClick}>Toggle Menu</button>
-          </Html>
         </scene>
       </Canvas>
       <canvas
@@ -58,20 +35,3 @@ function App() {
 }
 
 export default App;
-
-function Menu({ handleObjectClick }) {
-  // Menu component content
-  return (
-    <group>
-      <Html>
-        <div style={{ position: 'absolute', top: '50px', left: '50px' }}>
-          <h2>Menu</h2>
-          <button onClick={handleObjectClick}>Interact with Object</button>
-        </div>
-      </Html>
-    </group>
-  );
-}
-
-
-
